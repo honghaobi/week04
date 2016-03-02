b018205920f859c23692f18c1a4108b3e9457afe
henrybi <henrybi@u.washington.edu>
2016-03-02 13:08:21 -0800

    finished ajax assignment

--- XMLHttpRequest-exercises/script.js
+++ XMLHttpRequest-exercises/script.js
@@ -1,1 +1,50 @@
-console.log('You\'re ready to begin!')
+console.log('You\'re ready to begin!');
+
+var xhr = $.ajax({
+  method: "GET",
+  url: "index.html"
+});
+
+xhr.done(function(data) {
+    console.log(data);
+});
+
+xhr.fail(function(err){
+  console.log("FAIL");
+  console.log(err);
+});
+
+var xhr = $.ajax({
+  method: "GET",
+  dataType: 'json',
+  url: "data.json"
+});
+
+xhr.done(function(data) {
+    console.log(data);
+});
+
+xhr.fail(function(err){
+  console.log("FAIL");
+  console.log(err);
+});
+
+var xhr = $.ajax({
+  method: "GET",
+  dataType: 'json',
+  url: "http://www.reddit.com/r/aww.json"
+});
+
+xhr.done(function(data) {
+    console.log(data.data.children[0].data);
+    console.log(data.data.children[0].data.author);
+    console.log(data.data.children[0].data.title);
+    console.log(data.data.children[0].data.url);
+    var img = data.data.children[0].data.url.slice(0, -1);
+    $('.img').append("<img src='" + img + "'/>");
+});
+
+xhr.fail(function(err){
+  console.log("FAIL");
+  console.log(err);
+});
