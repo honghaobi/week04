30ac66c8f14d4ea95b52d9e2822e7c3dc1ef95ed
henrybi <henrybi@u.washington.edu>
2016-03-03 11:52:40 -0800

    cloned the front end api and spotify tracks assignment

---
+++ front-end-api-examples/app.js
@@ -0,0 +1,71 @@
+(function(){
+  $(document).ready(function(){
+
+    imdbSearch();
+    googleBooksSearch();
+    itunes();
+
+  })
+})();
+
+
+function imdbSearch(){
+
+  var userInput, searchUrl,results;
+  var outputArea = $("div#omdb .results");
+
+  $('#imdbButton').on("click", function(){
+     userInput = $('#imdbInput').val();
+     searchUrl = "http://www.omdbapi.com/?s=" + userInput
+
+     $.get(searchUrl, function( data ) {
+       results = data.Search;
+       results.forEach(function(item){
+         outputArea.append("<p>" + item.Title + "</p>")
+       })
+     });
+
+  })
+}
+
+
+function googleBooksSearch(){
+
+  var userInput, searchUrl, results;
+  var outputArea = $("div#googlebooks .results");
+
+  $('#googlebooksButton').on("click", function(){
+     userInput = $('#googlebooksInput').val();
+     searchUrl = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + userInput
+
+     $.get(searchUrl, function( data ) {
+       results = data.items;
+       results.forEach(function(item){
+         outputArea.append("<li>" + item.volumeInfo.title + "<br> Pages:" + item.volumeInfo.pageCount + "</li>")
+       })
+     });
+
+  })
+}
+
+function itunes(){
+
+  var userInput, searchUrl, results;
+  var outputArea = $("div#itunes .results");
+
+  $('#itunesButton').on("click", function(){
+     userInput = $('#itunesInput').val();
+     searchUrl = "https://itunes.apple.com/search?term=" + userInput
+
+      $.ajax({
+          type: "GET",
+          dataType:"jsonp",
+          url: searchUrl
+      }).then(function (data) {
+          results = data.results;
+          results.forEach(function(item){
+            outputArea.append("<li>" + item.artistName + " - " + item.trackName + " <a href='" + item.previewUrl + "'>Preview Song</a></li>")
+          })
+      });
+  });
+}
---
+++ front-end-api-examples/readme.md
@@ -0,0 +1,15 @@
+### Frontend API Examples
+
+- omdb
+- Google books
+- Twitter URL Shared Count
+- iTunes
+
+
+### Setup
+
+- Clone/Fork, in directory, run `python -m SimpleHTTPServer`
+
+### Issues
+
+- (FIXED)CORS Issues with Twitter URL Shared Counter
