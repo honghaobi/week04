8d8f4c9494c184338793838d9dc9d5107f364aaa
henrybi <henrybi@u.washington.edu>
2016-03-04 10:13:13 -0800

    added ajax call successful and also created append menu to order page function

--- galvanize-eats/app.js
+++ galvanize-eats/app.js
@@ -11,19 +11,21 @@ $(document).ready(function() {
      console.log("DONE");
      console.log(data);
      appendMenu(data);
-     console.log(data.menu[0].id);
 
   });
 
   xhr.fail(function(err){
-   console.log("FAIL");
+   console.log("fail");
    console.log(err);
   });
 
   var appendMenu = function(data){
     var $menu = $('.menu');
     for (var i = 0; i < data.menu.length; i++) {
-      $menu.append(data.menu[i].id);
+      $menu.append('<div class="menuID">' + data.menu[i].id + '</div>');
+      $menu.append('<div class="menuName">' + data.menu[i].name + '</div>');
+      $menu.append('<div class="menuPrice">' + data.menu[i].price + '</div>');
+      $menu.append('<div class="menuType">' + data.menu[i].type + '</div>');
     }
 
   };
