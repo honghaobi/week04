69030d94d99b6cc0f1348c3b7f16d97260aa1dd7
henrybi <henrybi@u.washington.edu>
2016-03-04 11:38:24 -0800

    created postCall

--- galvanize-eats/app.js
+++ galvanize-eats/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   var xhr = $.ajax({
     dataType: 'json',
     method: "GET",
@@ -8,7 +7,6 @@ $(document).ready(function() {
   });
 
   xhr.done(function(data) {
-     console.log("DONE");
      console.log(data);
      appendMenu(data);
 
@@ -20,18 +18,52 @@ $(document).ready(function() {
   });
 
   var appendMenu = function(data){
-    var $menu = $('.menu');
+    var $menuBurger = $('.menuBurger');
+    var $menuPizza = $('.menuPizza');
     for (var i = 0; i < data.menu.length; i++) {
-      $menu.append('<div class="menuID">' + data.menu[i].id + '</div>');
-      $menu.append('<div class="menuName">' + data.menu[i].name + '</div>');
-      $menu.append('<div class="menuPrice">' + data.menu[i].price + '</div>');
-      $menu.append('<div class="menuType">' + data.menu[i].type + '</div>');
+      if (data.menu[i].type === 'burger') {
+        $menuBurger.append('<div class="menuName">' + data.menu[i].name + '</div>');
+        $menuBurger.append('<div class="menuPrice">' + data.menu[i].price + '</div>');
+        $menuBurger.append('<div class="menuQuantity"> Quantity: ' + '<input id="item' + data.menu[i].id + '" type="text" size="3" value="0"' + '</div>');
+        $menuBurger.append('<button class="addItem' + data.menu[i].id + '" type="button" name="button">Add</button>');
+      } else if (data.menu[i].type === 'pizza') {
+        $menuPizza.append('<div class="menuName">' + data.menu[i].name + '</div>');
+        $menuPizza.append('<div class="menuPrice">' + data.menu[i].price + '</div>');
+        $menuPizza.append('<div class="menuQuantity"> Quantity: ' + '<input id="item' + data.menu[i].id + '" type="text" size="3" value="0"' + '</div>');
+        $menuPizza.append('<button class="addItem' + data.menu[i].id + '" type="button" name="button">Add</button>');
+      }
     }
-
   };
 
+  $('button.addItem').on('click', function() {
+    $('.orderList').append('<h7>CheeseBurger</h7><h8>$10.99</h8>');
+  });
 
 
 
+
+
+  var sendOrder = function(){
+    var xhr = $.ajax({
+      method: "POST",
+      url: "https://galvanize-eats-api.herokuapp.com/orders"
+    });
+
+    xhr.done(function(data) {
+       console.log(data);
+       appendMenu(data);
+
+    });
+
+    xhr.fail(function(err){
+     console.log("fail");
+     console.log(err);
+    });
+  };
+
+  $("#orderSubmit").on('click', function(event) {
+    event.preventDefault();
+    sendOrder();
+  });
 
 });
